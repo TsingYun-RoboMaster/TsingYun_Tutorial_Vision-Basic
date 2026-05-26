2f65272f67389876b4235e6f387a30e2e28d9797
Lijunze-tsinghua <lijz25@mails.tsinghua.edu.cn>
2026-05-26 12:11:02 +0800

    feat: 完成 ROS 2 话题通信作业 - 李俊泽

--- basic_ws/src/basic_topic/src/publisher_component.cpp
+++ basic_ws/src/basic_topic/src/publisher_component.cpp
@@ -1,43 +1,27 @@
-#include "basic_topic/publisher_component.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include <chrono>
 
-using namespace std::chrono_literals;
+class PublisherNode : public rclcpp::Node {
+public:
+  PublisherNode() : Node("publisher_node") {
+    publisher_ = this->create_publisher<std_msgs::msg::String>("chat_topic", 10);
+    timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&PublisherNode::timer_callback, this));
+  }
+private:
+  void timer_callback() {
+    auto message = std_msgs::msg::String();
+    message.data = "李俊泽 - 2025013550: Hello ROS 2!";
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+};
 
-namespace basic_topic
-{
-
-    PublisherComponent::PublisherComponent(const rclcpp::NodeOptions& options) :
-        Node("publisher_node", options)
-    {
-        // TODO
-    }
-
-    double PublisherComponent::normalize_angle(double angle)
-    {
-        angle = std::fmod(angle + kPi, 2.0 * kPi);
-        if (angle < 0.0)
-            angle += 2.0 * kPi;
-        return angle - kPi;
-    }
-
-    geometry_msgs::msg::Quaternion PublisherComponent::rpy_to_quaternion(double roll, double pitch, double yaw)
-    {
-        geometry_msgs::msg::Quaternion q;
-        const double cy = std::cos(yaw * 0.5);
-        const double sy = std::sin(yaw * 0.5);
-        const double cp = std::cos(pitch * 0.5);
-        const double sp = std::sin(pitch * 0.5);
-        const double cr = std::cos(roll * 0.5);
-        const double sr = std::sin(roll * 0.5);
-
-        q.w = cr * cp * cy + sr * sp * sy;
-        q.x = sr * cp * cy - cr * sp * sy;
-        q.y = cr * sp * cy + sr * cp * sy;
-        q.z = cr * cp * sy - sr * sp * cy;
-        return q;
-    }
-
-    // TODO
-
-}  // namespace basic_topic
-
-RCLCPP_COMPONENTS_REGISTER_NODE(basic_topic::PublisherComponent)
+int main(int argc, char * argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PublisherNode>());
+  rclcpp::shutdown();
+  return 0;
+}
--- basic_ws/src/basic_topic/src/subscriber_component.cpp
+++ basic_ws/src/basic_topic/src/subscriber_component.cpp
@@ -1,41 +1,22 @@
-#include "basic_topic/subscriber_component.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
-namespace basic_topic
-{
+class SubscriberNode : public rclcpp::Node {
+public:
+  SubscriberNode() : Node("subscriber_node") {
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "chat_topic", 10, std::bind(&SubscriberNode::topic_callback, this, std::placeholders::_1));
+  }
+private:
+  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const {
+    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
 
-    SubscriberComponent::SubscriberComponent(const rclcpp::NodeOptions& options) :
-        Node("subscriber_node", options)
-    {
-        // TODO
-    }
-
-    double SubscriberComponent::normalize_angle(double angle)
-    {
-        angle = std::fmod(angle + kPi, 2.0 * kPi);
-        if (angle < 0.0)
-            angle += 2.0 * kPi;
-        return angle - kPi;
-    }
-
-    void SubscriberComponent::quaternion_to_rpy(const geometry_msgs::msg::Quaternion& q, double& roll, double& pitch, double& yaw)
-    {
-        const double sinr_cosp = 2.0 * (q.w * q.x + q.y * q.z);
-        const double cosr_cosp = 1.0 - 2.0 * (q.x * q.x + q.y * q.y);
-        roll = std::atan2(sinr_cosp, cosr_cosp);
-
-        const double sinp = 2.0 * (q.w * q.y - q.z * q.x);
-        if (std::abs(sinp) >= 1.0)
-            pitch = std::copysign(kPi / 2.0, sinp);
-        else
-            pitch = std::asin(sinp);
-
-        const double siny_cosp = 2.0 * (q.w * q.z + q.x * q.y);
-        const double cosy_cosp = 1.0 - 2.0 * (q.y * q.y + q.z * q.z);
-        yaw = std::atan2(siny_cosp, cosy_cosp);
-    }
-
-    // TODO
-
-}  // namespace basic_topic
-
-RCLCPP_COMPONENTS_REGISTER_NODE(basic_topic::SubscriberComponent)
+int main(int argc, char * argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<SubscriberNode>());
+  rclcpp::shutdown();
+  return 0;
+}
